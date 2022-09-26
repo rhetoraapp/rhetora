7257d0ad340e40be8169ed79f813ab09d8e69231
lukaeric14 <lukaericnu@gmail.com>
2022-09-26 12:06:50 -0500

    server settings

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const api = require('./routes/routes.js');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
