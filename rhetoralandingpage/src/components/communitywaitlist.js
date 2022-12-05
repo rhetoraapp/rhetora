5679a069dee4f945345c724a84edbbbab158e330
lukaeric14 <lukaericnu@gmail.com>
2022-12-05 09:31:03 +0400

    form edits

---
+++ rhetoralandingpage/src/components/communitywaitlist.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import $ from 'jquery';
+
+const communitywaitlist = () => {
+
+    return (
+      <div>
+      <input></input>
+      <button>submit</button>
+      </div>
+    );
+  };
+  
+  export default communitywaitlist;
