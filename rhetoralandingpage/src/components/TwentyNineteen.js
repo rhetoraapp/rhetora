709187902dc8c0f36d78d5922e02b66f954470dd
anwarkhan0 <anwarsyed01234@gmail.com>
2023-01-11 22:47:48 +0500

    elements converted to svgs

--- rhetoralandingpage/src/components/TwentyNineteen.js
+++ rhetoralandingpage/src/components/TwentyNineteen.js
@@ -1,17 +1,32 @@
 import React from "react";
-import luka from "../assets/luka-line.png";
+import luca from "../assets/luka-line.png";
+import mazen from "../assets/mazen-line.png";
 function TwentyNineteen() {
   return (
-    <div className="d-flex justify-content-end row">
-      <img src={luka} alt="luka" className=" h-auto" />
-      <div className="">
-        Luka and Mazen have been business partners for many years, having met in
-        high school and bonded over our shared mentality and entrepreneurial
-        spirit. We have always been driven to find ways to improve upon existing
-        ideas and create new solutions to problems.
-      </div>
-      
-    </div>
+    <div className="d-flex w-full">
+            <div style={{ width: "50%" }} id="TwntyNntnStory">
+              <div className="tr">
+                <img src={luca} alt="" />
+              </div>
+              <p id="name" className="">
+                Luka
+              </p>
+              <div className="tc">
+                <div className="year">2019</div>
+                <div className="yearDescription">
+                  Luka and Mazen have been business partners for many years,
+                  having met in high school and bonded over our shared mentality
+                  and entrepreneurial spirit. We have always been driven to find
+                  ways to improve upon existing ideas and create new solutions
+                  to problems.
+                </div>
+              </div>
+              <div className="tr">
+                <img src={mazen} alt="mazen" />
+              </div>
+              <p id="name">mazen</p>
+            </div>
+          </div>
   );
 }
 
--- rhetoralandingpage/src/pages/Ourstory.js
+++ rhetoralandingpage/src/pages/Ourstory.js
@@ -1,120 +1,65 @@
-import {React,useEffect} from "react";
+import { React } from "react";
 import Navbar from '../components/navbar';
-import Footer from '../components/footer';
-import Background from '../assets/Hero-background.svg';
-import ReactGA from "react-ga";
-import luca from "../assets/luka-line.png";
-import mazen from "../assets/mazen-line.png";
-import topline from "../assets/topline.png";
-import vector145 from "../assets/vector145.png";
-import lucaPic from "../assets/lucapic.png";
-import mazePic from "../assets/mazepic.png";
-import bottomLine from "../assets/bottom-line.png";
+import Background from "../assets/Hero.png";
 import "../styles/ourstory.css";
-
-const Contactus = () => {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname);
-  },[]);
-  
-    return (
-      <div>
-        <Navbar/>
-        <div>
+import LukaLine from "../components/ourstory/lukaLine";
+import MazeLine from "../components/ourstory/mazeLine";
+import TopLine from "../components/ourstory/Topline";
+import Vector145 from "../components/ourstory/Vector145";
+import V1 from "../components/ourstory/V1";
+import BottomLine from "../components/ourstory/BottomLine";
+import Text3 from "../components/ourstory/Text3";
+import V4 from "../components/ourstory/V4";
+import Text2021 from "../components/ourstory/Text2021";
+const Ourstory = () => {
+  return (
+    <div>
+      <Navbar />
       <div
-        className="d-flex justify-content-end row"
         style={{
-          height: "100vh",
-          backgroundImage: `url(${Background})`,
-          backgroundSize: "cover",
-          backgroundRepeat: "repeat-y",
+          position: "relative",
+          top: "0px",
+          maxWidth: "1512px",
+          height: "3203px",
+          background: `url(${Background})`,
         }}
       >
-        <div className="d-flex w-full">
-          <div style={{ width: "50%" }} id="TwntyNntnStory">
-            <div className="tr">
-              <img src={luca} alt="" />
-            </div>
-            <p id="name" className="">
-              Luka
-            </p>
-            <div className="tc">
-              <div className="year">2019</div>
-              <div className="yearDescription">
-                Luka and Mazen have been business partners for many years,
-                having met in high school and bonded over our shared mentality
-                and entrepreneurial spirit. We have always been driven to find
-                ways to improve upon existing ideas and create new solutions to
-                problems.
-              </div>
-            </div>
-            <div className="tr">
-              <img src={mazen} alt="mazen" />
-            </div>
-            <p id="name">mazen</p>
-          </div>
-        </div>
-      </div>
-
-      <div className="">
-        <div className="tr">
-          <img src={topline} alt="topline" />
-          <img src={vector145} id="vector145" />
-        </div>
-
-        <div id="topRect">
-          <div className="v1">
-            <div id="percRectangle">
-              %97
-              <br />
-              Co-Founder
-              <br />
-              Match
-            </div>
-
-            <div id="matchRectangle">
-              <img src={lucaPic} id="pic" />
-              <span className="flex justify-center">
-                <div id="barone"></div>
-                <div id="bartwo"></div>
-              </span>
-              <img src={mazePic} id="pic" />
-            </div>
-          </div>
-        </div>
-
-        <img src={bottomLine} id="bottomLine" />
-
-        <div className="" id="Twenty22">
-          <div className="year" id="year22">2022</div>
-          <div className="yearDescription">
+        <LukaLine id="lucaLine" />
+        <div>
+          <div id="heading1">2019</div>
+          <div id="description1">
             Luka and Mazen have been business partners for many years, having
             met in high school and bonded over our shared mentality and
             entrepreneurial spirit. We have always been driven to find ways to
             improve upon existing ideas and create new solutions to problems.
           </div>
         </div>
+        <MazeLine id="mazeLine" />
 
-        <div></div>
-        {/* right section area */}
-        <div className="tr" id="rightSec">
-          <div id="yearTwenty21">
-              2020&2021
-          </div>
-          <div id="yearTwenty21Desc">
-          Luka and I started our journey as e-commerce partners, building and growing our online business. This allowed us to gain valuable experience and insights into the world of entrepreneurship and e-commerce.
-          </div>
+        <TopLine id="topline" />
+        <Vector145 id="vector145" />
+        <V1 id="v1" />
+        <BottomLine id="bottomline" />
 
-          <div id="rectangle21">
-              <div id="v4">
-              </div>
+        <div>
+          <div id="heading3">2023</div>
+          <div id="description3">
+            During the growth of our e-commerce business and working in sales,
+            we experienced the challenges of cold calling and decided to create
+            Rhetora, an AI-powered platform that helps salespeople improve their
+            cold calling skills and increase their chances of success in a
+            stress-free environment.
           </div>
         </div>
+
+        <Text3 id="text3" />
+
+        <V4 id="v4" />
+
+        <Text2021 id="textRight" />
       </div>
     </div>
-        <Footer/>
-      </div>
-    );
-  };
-   
-  export default Contactus;
+  );
+};
+
+export default Ourstory;
