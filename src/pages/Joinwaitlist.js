bc14cb073179dd5bb03a471f95bbe9902929eb5a
ayabdi <abdulrahmanabdi98@gmail.com>
2023-03-03 14:44:50 +0300

    responsive layout complete

--- src/pages/Joinwaitlist.js
+++ src/pages/Joinwaitlist.js
@@ -11,24 +11,23 @@ const Join = () => {
     ReactGA.pageview(window.location.pathname);
   }, []);
 
-  const textGradientStyle = (background) => ({
-    WebkitBackgroundClip: "text",
-    backgroundClip: "text",
-    color: "transparent",
-    WebkitTextFillColor: "transparent",
-    background,
-  });
+  const textGradientStyle = (background) => {
+    return {
+      background,
+      WebkitBackgroundClip: "text",
+      backgroundClip: "text",
+      color: "transparent",
+    };
+  };
 
   return (
     <div>
       <Navbar />
-      <div
-        style={{ height: "100vh", backgroundImage: `url(${Background})`, backgroundSize: "cover" }}
-      >
-        <div className="d-flex justify-content-center">
-          <div className="col-8 mt-5 d-flex justify-center flex-column">
+      <div style={{ height: "100vh", backgroundImage: `url(${Background})`, backgroundSize: "cover" }}>
+        <div className="justify-content-center mx-auto max-w-[90vw] lg:max-w-[1000px]">
+          <div className=" d-flex flex-column mt-5 justify-center">
             <h1
-              className="tc mt-5 text-5xl"
+              className="tc text-3xl md:mt-5 md:text-5xl"
               style={{
                 ...textGradientStyle(
                   "radial-gradient(50.31% 50.31% at 51.49% 49.69%, rgba(255, 255, 255, 0.6) 0%, rgba(255, 255, 255, 0) 100%), linear-gradient(87.84deg, #FFBE0B 14.27%, #FF9933 26.9%, rgba(255, 153, 51, 0.85) 37.9%, rgba(255, 0, 110, 0.77) 49.83%, rgba(131, 56, 236, 0.799403) 59.71%, #8338EC 69.42%, #3A86FF 85.23%)"
@@ -38,15 +37,15 @@ const Join = () => {
               March 19th 2023. <br />
               rhetora.ai Unleashed.
             </h1>
-            <div className="mt-4 flex flex-col h-[55vh]">
-              <div className="flex flex-row basis-3/4 flex-1 mb-3">
-                <div className="bg-graylight rounded-[17px] basis-1/4 text-center mr-3 p-3">
+            <div className="flex h-[55vh] flex-col md:mt-4">
+              <div className="flex flex-1 basis-3/4 flex-col md:flex-row ">
+                <div className="my-1.5 basis-1/4 rounded-[17px] bg-graylight p-3 text-center md:mr-3">
                   <div className="flex flex-col">
                     <div
                       className="tc text-5xl"
                       style={{
                         ...textGradientStyle(
-                          "linear-gradient(93.79deg, #FF9933 18.63%, #FD71AF 89.95%) font-bold"
+                          " linear-gradient(101.53deg, #FFBE0B 10.97%, #FF9933 33.08%, rgba(255, 153, 51, 0.85) 40.29%, rgba(255, 0, 110, 0.77) 49.79%, rgba(131, 56, 236, 0.799403) 58.63%, #8338EC 75.6%, #3A86FF 96%)"
                         ),
                       }}
                     >
@@ -55,20 +54,20 @@ const Join = () => {
                   </div>
                 </div>
 
-                <div className="flex flex-col basis-1/2">
-                  <div className="flex flex-row basis-1/3 rounded-[17px] text-center mr-3">
-                    <div className="bg-graylight rounded-[17px] flex-1 mr-3">s</div>
-                    <div className="bg-graylight rounded-[17px] flex-1"> s</div>
+                <div className="flex basis-1/2 flex-col">
+                  <div className="flex basis-1/3 flex-col rounded-[17px] text-center md:mr-3 md:flex-row">
+                    <div className="my-1.5 flex-1 rounded-[17px] bg-graylight md:mr-3"></div>
+                    <div className="my-1.5 flex-1 rounded-[17px] bg-graylight"> </div>
                   </div>
-                  <div className="bg-graylight rounded-[17px] flex-1 mt-3 mr-3"></div>
+                  <div className="my-1.5 flex-1 rounded-[17px] bg-graylight md:mr-3"></div>
                 </div>
 
-                <div className="bg-graylight rounded-[17px] basis-1/4 text-center">s</div>
+                <div className="my-1.5 basis-1/4 rounded-[17px] bg-graylight text-center"></div>
               </div>
-              <div className="flex flex-row basis-1/4">
-                <div className="bg-graylight rounded-[17px] flex-1 text-center mr-3">s</div>
-                <div className="bg-graylight rounded-[17px] flex-1 text-center mr-3">s</div>
-                <div className="bg-graylight rounded-[17px] flex-1 text-center ">s</div>
+              <div className="flex basis-1/4 flex-col md:flex-row ">
+                <div className="my-1.5 flex-1 rounded-[17px] bg-graylight text-center md:mr-3"></div>
+                <div className="my-1.5 flex-1 rounded-[17px] bg-graylight text-center md:mr-3"></div>
+                <div className="my-1.5 flex-1 rounded-[17px] bg-graylight text-center"></div>
               </div>
             </div>
           </div>
