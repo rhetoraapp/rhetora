963621d8c8a467b3f712d60fd04e47ec24083b60
ayabdi <abdulrahmanabdi98@gmail.com>
2023-03-06 20:02:07 +0300

    waitlist modal complete

--- src/components/WaitlistModal.js
+++ src/components/WaitlistModal.js
@@ -1,12 +1,16 @@
 import { Dialog, Transition } from "@headlessui/react";
-import { Fragment, useState, useEffect } from "react";
+import { Fragment, useState, useEffect, useRef } from "react";
+
+const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
 export const WaitlistModal = ({ isOpen, closeModal, email }) => {
-  const [emails, setEmails] = useState(["abdulrahman"]);
+  const [emails, setEmails] = useState([]);
 
   const [input, setInput] = useState("");
   const [waitlistCount, setWaitlistCount] = useState(100);
   const [points, setPoints] = useState(100);
+
+  const inputDivRef = useRef(null);
 
   const addEmail = (e) => {
     e.preventDefault();
@@ -14,12 +18,23 @@ export const WaitlistModal = ({ isOpen, closeModal, email }) => {
       setEmails([...emails, input]);
     }
     setInput("");
+
+    inputDivRef.current.scrollleft += 200;
   };
 
   useEffect(() => {
     // add 100 points for every email. max 1000 points
     setPoints(Math.min((emails.length + 1) * 100, 1000));
   }, [emails]);
+
+  useEffect(() => {
+    // if input is valid email and there is a space at the end of the input, add email.
+    // regex to check if input is valid email
+    if (input.endsWith(" ") && input.trim().match(EMAIL_REGEX)) {
+      setEmails([...emails, input.trim()]);
+      setInput("");
+    }
+  }, [input]);
 
   return (
     <>
@@ -62,7 +77,7 @@ export const WaitlistModal = ({ isOpen, closeModal, email }) => {
                     </p>
                   </div>
                   <div className="mx-auto mt-4 mb-1.5 flex h-11 w-full max-w-lg justify-between rounded-md border bg-white px-2.5 shadow">
-                    <div className="flex h-full w-full flex-row overflow-x-auto">
+                    <div className="invite-waitlist-input-div mr-4 flex h-full w-full flex-row  overflow-x-auto" ref={inputDivRef}>
                       {emails.map((email) => (
                         <div className="my-auto mx-1 h-8 items-center rounded bg-graylight px-2 pt-2">
                           <p className="text-gray-500 my-auto flex text-base">
@@ -76,13 +91,14 @@ export const WaitlistModal = ({ isOpen, closeModal, email }) => {
 
                       <form onSubmit={addEmail} className="h-full w-full">
                         <input
-                          className="h-full w-full flex-1 border-none bg-transparent px-2 pt-1 leading-3 outline-none"
+                          className="h-full w-full min-w-max flex-1 border-none bg-transparent px-2 pt-1 leading-3 outline-none"
                           value={input}
                           onChange={(e) => setInput(e.target.value)}
+                          placeholder="Enter an email address"
                         />
                       </form>
                     </div>
-                    <button className="my-auto h-6 rounded bg-minor px-3 py-0.5 text-base text-white" onClick={addEmail}>
+                    <button className="my-auto h-7 rounded bg-minor px-3 py-1 text-base text-white" onClick={addEmail}>
                       Invite
                     </button>
                   </div>
@@ -99,6 +115,42 @@ export const WaitlistModal = ({ isOpen, closeModal, email }) => {
 
                       <ProgressBar points={points} />
                     </div>
+                  </div>
+
+                  <div className="mx-auto mt-4 mb-1.5 flex w-full max-w-lg justify-between rounded-md border bg-white p-3 shadow">
+                    <ul className="w-full list-inside list-disc">
+                      <li className="flex w-full  py-2">
+                        <p className="list-item flex-1 text-lg">Get Early Access</p>
+                        <p className="text-lg">100</p>
+                      </li>
+                      {/*Strike through  */}
+                      {points >= 100 && <div className="-mt-[25px] mb-4 ml-auto h-0.5 w-[95%] bg-black" />}
+
+                      <li className="flex w-full items-center justify-between py-2">
+                        <p className="list-item flex-1 text-lg">Get Earlier Access</p>
+                        <p className="text-lg">200</p>
+                      </li>
+                      {/*Strike through  */}
+                      {points >= 200 && <div className="-mt-[25px] mb-4 ml-auto h-0.5 w-[95%] bg-black" />}
+
+                      <li className="flex w-full items-center justify-between py-2">
+                        <p className="list-item text-lg">Get First Month Free</p>
+                        <p className="text-lg">300</p>
+                      </li>
+                      {/*Strike through  */}
+                      {points >= 300 && <div className="-mt-[25px] mb-4 ml-auto h-0.5 w-[95%] bg-black" />}
+                      <li className="flex w-full items-center justify-between py-2">
+                        <p className="list-item text-lg">Get 3 Months free</p>
+                        <p className="text-lg">500</p>
+                      </li>
+                      {/*Strike through  */}
+                      {points >= 500 && <div className="-mt-[25px] mb-4 ml-auto h-0.5 w-[95%] bg-black" />}
+                      <li className="flex w-full items-center justify-between py-2">
+                        <p className="list-item text-lg">Get Rhetora Free for Life</p>
+                        <p className="text-lg">1000</p>
+                      </li>
+                      {points >= 1000 && <div className="-mt-[25px] mb-4 ml-auto h-0.5 w-[95%] bg-black" />}
+                    </ul>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
@@ -120,7 +172,7 @@ const ProgressBar = ({ points }) => {
         dot === 1 && "before:rounded-l-full"
       }`}
     >
-      <img src={require("../assets/checkmark-orange.svg").default} className={`h-[22px] ${dot === 1 ? "-mr-0.5" : "-mr-2"}`} />
+      <img src={require("../assets/checkmark-orange.svg").default} className={`h-[22px] ${dot === 1 ? "-mr-2" : "-mr-2"}`} />
     </li>
   );
 
@@ -141,7 +193,10 @@ const ProgressBar = ({ points }) => {
   );
 
   const renderIncompleteDot = (dot) => (
-    <li key={dot} className={dot !== 1 && "flex w-full items-center justify-center text-center before:inline-block before:w-full"}>
+    <li
+      key={dot}
+      className={`flex w-full items-center justify-center text-center ${dot !== 1 ? "before:inline-block before:w-full" : "-ml-2.5 before:w-1/2"}`}
+    >
       <div className="h-2 w-2 shrink-0 items-center rounded-full bg-minor"></div>
     </li>
   );
@@ -158,12 +213,12 @@ const ProgressBar = ({ points }) => {
 
   return (
     <div className="mt-3">
-      <ol className="flex h-8 w-full items-center justify-between rounded bg-minorlight px-3">{dots.map(renderDot)}</ol>
+      <ol className="flex h-8 w-full items-center justify-between rounded bg-minorlight px-3 !pr-5">{dots.map(renderDot)}</ol>
 
       {/* display points below the bar */}
-      <div className="text-gray-500 mt-1.5 ml-1.5 flex w-full justify-between px-2.5 text-sm">
+      <div className="text-gray-500 mt-1.5 ml-1.5 flex w-full justify-between pl-3 pr-2.5 text-sm ">
         {dots.map((dot) => (
-          <p className={`text-sm font-bold ${dot === 1 ? "ml-3 mr-1" : " ml-auto"} `} key={dot}>
+          <p className={`text-sm font-bold ${dot === 1 ? "ml-auto" : " ml-auto"} `} key={dot}>
             {dot * 100}
           </p>
         ))}
